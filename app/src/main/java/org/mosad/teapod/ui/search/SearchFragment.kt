b72b316ce1078552dbad349943c4698a317873b9
Jannik <seil0@mosad.xyz>
2020-10-11 14:25:47 +0200

    clean up build-gradle
    * remove unused dependencies

--- app/src/main/java/org/mosad/teapod/ui/search/SearchFragment.kt
+++ app/src/main/java/org/mosad/teapod/ui/search/SearchFragment.kt
@@ -4,28 +4,18 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProviders
+import kotlinx.android.synthetic.main.fragment_search.*
 import org.mosad.teapod.R
 
 class SearchFragment : Fragment() {
 
-    private lateinit var notificationsViewModel: SearchViewModel
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_search, container, false)
+    }
 
-    override fun onCreateView(
-            inflater: LayoutInflater,
-            container: ViewGroup?,
-            savedInstanceState: Bundle?
-    ): View? {
-        notificationsViewModel =
-                ViewModelProviders.of(this).get(SearchViewModel::class.java)
-        val root = inflater.inflate(R.layout.fragment_search, container, false)
-        val textView: TextView = root.findViewById(R.id.text_notifications)
-        notificationsViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
-        })
-        return root
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        text_search.text = "This is the search Fragment"
     }
 }
