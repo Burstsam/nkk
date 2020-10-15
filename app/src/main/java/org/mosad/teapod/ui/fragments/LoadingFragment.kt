a8dc243d0ead7e9e2886ece10880e9b790e07641
Jannik <seil0@mosad.xyz>
2020-10-15 19:01:37 +0200

    move all fragments into the fragments package

--- app/src/main/java/org/mosad/teapod/ui/fragments/LoadingFragment.kt
+++ app/src/main/java/org/mosad/teapod/ui/fragments/LoadingFragment.kt
@@ -1,4 +1,4 @@
-package org.mosad.teapod.util
+package org.mosad.teapod.ui.fragments
 
 import android.os.Bundle
 import android.view.LayoutInflater
--- app/src/main/java/org/mosad/teapod/ui/fragments/SearchFragment.kt
+++ app/src/main/java/org/mosad/teapod/ui/fragments/SearchFragment.kt
@@ -1,4 +1,4 @@
-package org.mosad.teapod.ui.search
+package org.mosad.teapod.ui.fragments
 
 import android.os.Bundle
 import android.view.LayoutInflater
