Burstsam/nkk

commit 7df99ea0cc5923ad1989018417cab364d663d6fb

use view binding wherever possible

app/src/main/java/org/mosad/teapod/MainActivity.kt
@@ -30,10 +30,11 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.commit
 import com.google.android.material.bottomnavigation.BottomNavigationView
-import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
+import org.mosad.teapod.databinding.ActivityMainBinding
 import org.mosad.teapod.parser.AoDParser
+import org.mosad.teapod.player.PlayerActivity
 import org.mosad.teapod.preferences.EncryptedPreferences
 import org.mosad.teapod.preferences.Preferences
 import org.mosad.teapod.ui.components.LoginDialog
@@ -45,6 +46,7 @@ import kotlin.system.measureTimeMillis
 
 class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemSelectedListener {
 
+    private lateinit var binding: ActivityMainBinding
     private var activeBaseFragment: Fragment = HomeFragment() // the currently active fragment, home at the start
 
     companion object {
@@ -53,6 +55,7 @@ class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemS
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
 
         if (!wasInitialized) {
             load()
@@ -60,8 +63,8 @@ class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemS
 
         theme.applyStyle(getThemeResource(), true)
 
-        setContentView(R.layout.activity_main)
-        nav_view.setOnNavigationItemSelectedListener(this)
+        setContentView(binding.root)
+        binding.navView.setOnNavigationItemSelectedListener(this)
 
         supportFragmentManager.commit {
             replace(R.id.nav_host_fragment, activeBaseFragment, activeBaseFragment.javaClass.simpleName)
@@ -73,7 +76,7 @@ class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemS
             supportFragmentManager.popBackStack()
         } else {
             if (activeBaseFragment !is HomeFragment) {
-                nav_view.selectedItemId = R.id.navigation_home
+                binding.navView.selectedItemId = R.id.navigation_home
             } else {
                 super.onBackPressed()
             }

app/src/main/java/org/mosad/teapod/ui/fragments/MediaFragment.kt
@@ -13,9 +13,9 @@ import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.bumptech.glide.request.RequestOptions
 import jp.wasabeef.glide.transformations.BlurTransformation
-import kotlinx.android.synthetic.main.fragment_media.*
 import org.mosad.teapod.MainActivity
 import org.mosad.teapod.R
+import org.mosad.teapod.databinding.FragmentMediaBinding
 import org.mosad.teapod.parser.AoDParser
 import org.mosad.teapod.util.DataTypes.MediaType
 import org.mosad.teapod.util.Episode
@@ -26,6 +26,7 @@ import org.mosad.teapod.util.adapter.EpisodeItemAdapter
 
 class MediaFragment(private val media: Media, private val tmdb: TMDBResponse) : Fragment() {
 
+    private lateinit var binding: FragmentMediaBinding
     private lateinit var adapterRecEpisodes: EpisodeItemAdapter
     private lateinit var viewManager: RecyclerView.LayoutManager
     private lateinit var nextEpisode: Episode
@@ -38,8 +39,9 @@ class MediaFragment(private val media: Media, private val tmdb: TMDBResponse) : 
         instance = this
     }
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.fragment_media, container, false)
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+        binding = FragmentMediaBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -60,29 +62,29 @@ class MediaFragment(private val media: Media, private val tmdb: TMDBResponse) : 
         Glide.with(requireContext()).load(backdropUrl)
             .apply(RequestOptions.placeholderOf(ColorDrawable(Color.DKGRAY)))
             .apply(RequestOptions.bitmapTransform(BlurTransformation(20, 3)))
-            .into(image_backdrop)
+            .into(binding.imageBackdrop)
 
         Glide.with(requireContext()).load(posterUrl)
-            .into(image_poster)
+            .into(binding.imagePoster)
 
-        text_title.text = media.info.title
-        text_year.text = media.info.year.toString()
-        text_age.text = media.info.age.toString()
-        text_overview.text = media.info.shortDesc
+        binding.textTitle.text = media.info.title
+        binding.textYear.text = media.info.year.toString()
+        binding.textAge.text = media.info.age.toString()
+        binding.textOverview.text = media.info.shortDesc
         if (StorageController.myList.contains(media.id)) {
-            Glide.with(requireContext()).load(R.drawable.ic_baseline_check_24).into(image_my_list_action)
+            Glide.with(requireContext()).load(R.drawable.ic_baseline_check_24).into(binding.imageMyListAction)
         } else {
-            Glide.with(requireContext()).load(R.drawable.ic_baseline_add_24).into(image_my_list_action)
+            Glide.with(requireContext()).load(R.drawable.ic_baseline_add_24).into(binding.imageMyListAction)
         }
 
         // specific gui
         if (media.type == MediaType.TVSHOW) {
             adapterRecEpisodes = EpisodeItemAdapter(media.episodes)
             viewManager = LinearLayoutManager(context)
-            recycler_episodes.layoutManager = viewManager
-            recycler_episodes.adapter = adapterRecEpisodes
+            binding.recyclerEpisodes.layoutManager = viewManager
+            binding.recyclerEpisodes.adapter = adapterRecEpisodes
 
-            text_episodes_or_runtime.text = getString(R.string.text_episodes_count, media.info.episodesCount)
+            binding.textEpisodesOrRuntime.text = getString(R.string.text_episodes_count, media.info.episodesCount)
 
             // get next episode
             nextEpisode = if (media.episodes.firstOrNull{ !it.watched } != null) {
@@ -92,20 +94,20 @@ class MediaFragment(private val media: Media, private val tmdb: TMDBResponse) : 
             }
 
             // title is the next episodes title
-            text_title.text = nextEpisode.title
+            binding.textTitle.text = nextEpisode.title
         } else if (media.type == MediaType.MOVIE) {
-            recycler_episodes.visibility = View.GONE
+            binding.recyclerEpisodes.visibility = View.GONE
 
             if (tmdb.runtime > 0) {
-                text_episodes_or_runtime.text = getString(R.string.text_runtime, tmdb.runtime)
+                binding.textEpisodesOrRuntime.text = getString(R.string.text_runtime, tmdb.runtime)
             } else {
-                text_episodes_or_runtime.visibility = View.GONE
+                binding.textEpisodesOrRuntime.visibility = View.GONE
             }
         }
     }
 
     private fun initActions() {
-        button_play.setOnClickListener {
+        binding.buttonPlay.setOnClickListener {
             when (media.type) {
                 MediaType.MOVIE -> playStream(media.episodes.first())
                 MediaType.TVSHOW -> playEpisode(nextEpisode)
@@ -114,13 +116,13 @@ class MediaFragment(private val media: Media, private val tmdb: TMDBResponse) : 
         }
 
         // add or remove media from myList
-        linear_my_list_action.setOnClickListener {
+        binding.linearMyListAction.setOnClickListener {
             if (StorageController.myList.contains(media.id)) {
                 StorageController.myList.remove(media.id)
-                Glide.with(requireContext()).load(R.drawable.ic_baseline_add_24).into(image_my_list_action)
+                Glide.with(requireContext()).load(R.drawable.ic_baseline_add_24).into(binding.imageMyListAction)
             } else {
                 StorageController.myList.add(media.id)
-                Glide.with(requireContext()).load(R.drawable.ic_baseline_check_24).into(image_my_list_action)
+                Glide.with(requireContext()).load(R.drawable.ic_baseline_check_24).into(binding.imageMyListAction)
             }
             StorageController.saveMyList(requireContext())
 
@@ -153,7 +155,7 @@ class MediaFragment(private val media: Media, private val tmdb: TMDBResponse) : 
         } else {
             media.episodes.first()
         }
-        text_title.text = nextEpisode.title
+        binding.textTitle.text = nextEpisode.title
     }
 
     private fun playStream(ep: Episode) {

app/build.gradle
@@ -18,6 +18,10 @@ android {
         setProperty("archivesBaseName", "teapod-$versionName")
     }
 
+    buildFeatures {
+        viewBinding true
+    }
+
     buildTypes {
         release {
             minifyEnabled true

app/src/main/AndroidManifest.xml
@@ -22,7 +22,7 @@
             </intent-filter>
         </activity>
         <activity
-            android:name=".PlayerActivity"
+            android:name=".player.PlayerActivity"
             android:label="@string/app_name"
             android:theme="@style/PlayerTheme"
             android:configChanges="orientation|screenSize|layoutDirection" />

app/src/main/java/org/mosad/teapod/player/PlayerActivity.kt
@@ -1,4 +1,4 @@
-package org.mosad.teapod
+package org.mosad.teapod.player
 
 import android.animation.Animator
 import android.animation.AnimatorListenerAdapter
@@ -27,7 +27,7 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
-import org.mosad.teapod.player.PlayerViewModel
+import org.mosad.teapod.R
 import org.mosad.teapod.preferences.Preferences
 import org.mosad.teapod.util.Episode
 import java.util.*
@@ -36,13 +36,13 @@ import kotlin.concurrent.scheduleAtFixedRate
 
 class PlayerActivity : AppCompatActivity() {
 
+    private val model: PlayerViewModel by viewModels()
+
     private lateinit var player: SimpleExoPlayer
     private lateinit var dataSourceFactory: DataSource.Factory
     private lateinit var controller: StyledPlayerControlView
     private lateinit var gestureDetector: GestureDetectorCompat
     private lateinit var timerUpdates: TimerTask
-
-    private val model: PlayerViewModel by viewModels()
 
     private var playWhenReady = true
     private var currentWindow = 0
@@ -78,7 +78,7 @@ class PlayerActivity : AppCompatActivity() {
         super.onStart()
         if (Util.SDK_INT > 23) {
             initPlayer()
-            if (video_view != null) video_view.onResume()
+            video_view?.onResume()
         }
     }
 
@@ -86,14 +86,14 @@ class PlayerActivity : AppCompatActivity() {
         super.onResume()
         if (Util.SDK_INT <= 23) {
             initPlayer()
-            if (video_view != null) video_view.onResume()
+            video_view?.onResume()
         }
     }
 
     override fun onPause() {
         super.onPause()
         if (Util.SDK_INT <= 23) {
-            if (video_view != null) video_view.onPause()
+            video_view?.onPause()
             releasePlayer()
         }
     }

app/src/main/java/org/mosad/teapod/ui/fragments/AccountFragment.kt
@@ -9,10 +9,10 @@ import androidx.fragment.app.Fragment
 import com.afollestad.materialdialogs.MaterialDialog
 import com.afollestad.materialdialogs.list.listItemsSingleChoice
 import de.psdev.licensesdialog.LicensesDialog
-import kotlinx.android.synthetic.main.fragment_account.*
 import org.mosad.teapod.BuildConfig
 import org.mosad.teapod.MainActivity
 import org.mosad.teapod.R
+import org.mosad.teapod.databinding.FragmentAccountBinding
 import org.mosad.teapod.parser.AoDParser
 import org.mosad.teapod.preferences.EncryptedPreferences
 import org.mosad.teapod.preferences.Preferences
@@ -21,44 +21,46 @@ import org.mosad.teapod.util.DataTypes.Theme
 
 class AccountFragment : Fragment() {
 
+    private lateinit var binding: FragmentAccountBinding
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.fragment_account, container, false)
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+        binding = FragmentAccountBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        text_account_login.text = EncryptedPreferences.login
-        text_info_about_desc.text = getString(R.string.info_about_desc, BuildConfig.VERSION_NAME, getString(R.string.build_time))
-        text_theme_selected.text = when (Preferences.theme) {
+        binding.textAccountLogin.text = EncryptedPreferences.login
+        binding.textInfoAboutDesc.text = getString(R.string.info_about_desc, BuildConfig.VERSION_NAME, getString(R.string.build_time))
+        binding.textThemeSelected.text = when (Preferences.theme) {
             Theme.DARK -> getString(R.string.theme_dark)
             else -> getString(R.string.theme_light)
         }
 
-        switch_secondary.isChecked = Preferences.preferSecondary
-        switch_autoplay.isChecked = Preferences.autoplay
+        binding.switchSecondary.isChecked = Preferences.preferSecondary
+        binding.switchAutoplay.isChecked = Preferences.autoplay
 
         initActions()
     }
 
     private fun initActions() {
-        linear_account_login.setOnClickListener {
+        binding.linearAccountLogin.setOnClickListener {
             showLoginDialog(true)
         }
 
-        linear_theme.setOnClickListener {
+        binding.linearTheme.setOnClickListener {
             showThemeDialog()
         }
 
-        linear_about.setOnClickListener {
+        binding.linearInfo.setOnClickListener {
             MaterialDialog(requireContext())
                 .title(R.string.info_about)
                 .message(R.string.info_about_dialog)
                 .show()
         }
 
-        text_licenses.setOnClickListener {
+        binding.textLicenses.setOnClickListener {
 
             val dialogCss = when (Preferences.theme) {
                 Theme.DARK -> R.string.license_dialog_style_dark
@@ -80,12 +82,12 @@ class AccountFragment : Fragment() {
                 .show()
         }
 
-        switch_secondary.setOnClickListener {
-            Preferences.savePreferSecondary(requireContext(), switch_secondary.isChecked)
+        binding.switchSecondary.setOnClickListener {
+            Preferences.savePreferSecondary(requireContext(), binding.switchSecondary.isChecked)
         }
 
-        switch_autoplay.setOnClickListener {
-            Preferences.saveAutoplay(requireContext(), switch_autoplay.isChecked)
+        binding.switchAutoplay.setOnClickListener {
+            Preferences.saveAutoplay(requireContext(), binding.switchAutoplay.isChecked)
         }
     }
 

app/src/main/java/org/mosad/teapod/ui/fragments/HomeFragment.kt
@@ -5,13 +5,12 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import kotlinx.android.synthetic.main.fragment_home.*
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 import org.mosad.teapod.MainActivity
-import org.mosad.teapod.R
+import org.mosad.teapod.databinding.FragmentHomeBinding
 import org.mosad.teapod.parser.AoDParser
 import org.mosad.teapod.util.StorageController
 import org.mosad.teapod.util.adapter.MediaItemAdapter
@@ -19,15 +18,13 @@ import org.mosad.teapod.util.decoration.MediaItemDecoration
 
 class HomeFragment : Fragment() {
 
+    private lateinit var binding: FragmentHomeBinding
     private lateinit var adapterMyList: MediaItemAdapter
     private lateinit var adapterNewEpisodes: MediaItemAdapter
 
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_home, container, false)
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+        binding = FragmentHomeBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -36,13 +33,13 @@ class HomeFragment : Fragment() {
         GlobalScope.launch {
             withContext(Dispatchers.Main) {
                 context?.let {
-                    recycler_my_list.addItemDecoration(MediaItemDecoration(9))
+                    binding.recyclerMyList.addItemDecoration(MediaItemDecoration(9))
 
                     updateMyListMedia()
 
                     adapterNewEpisodes = MediaItemAdapter(AoDParser.newEpisodesList)
-                    recycler_new_episodes.adapter = adapterNewEpisodes
-                    recycler_new_episodes.addItemDecoration(MediaItemDecoration(9))
+                    binding.recyclerNewEpisodes.adapter = adapterNewEpisodes
+                    binding.recyclerNewEpisodes.addItemDecoration(MediaItemDecoration(9))
 
                     initActions()
                 }
@@ -63,7 +60,7 @@ class HomeFragment : Fragment() {
             (activity as MainActivity).showMediaFragment(mediaId)
         }
 
-        recycler_my_list.adapter = adapterMyList
+        binding.recyclerMyList.adapter = adapterMyList
     }
 
     private fun initActions() {

commit 2de1419d362abfd75e59d934f8a75b42275ec965

fix theme not applying to nav bar
regression from 7df99ea0cc

app/src/main/java/org/mosad/teapod/MainActivity.kt
@@ -55,16 +55,13 @@ class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemS
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
 
-        if (!wasInitialized) {
-            load()
-        }
-
+        if (!wasInitialized) { load() }
         theme.applyStyle(getThemeResource(), true)
 
-        setContentView(binding.root)
+        binding = ActivityMainBinding.inflate(layoutInflater)
         binding.navView.setOnNavigationItemSelectedListener(this)
+        setContentView(binding.root)
 
         supportFragmentManager.commit {
             replace(R.id.nav_host_fragment, activeBaseFragment, activeBaseFragment.javaClass.simpleName)
@@ -160,30 +157,15 @@ class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemS
 
     /**
      * Show the media fragment for the selected media.
-     * While loading show the loading fragment.
-     * The loading and media fragment are not stored in activeBaseFragment,
-     * as the don't replace a fragment but are added on top of one.
+     * The media fragment is not stored in activeBaseFragment,
+     * as it doesn't replace a fragment but is added on top of one.
      */
     fun showMediaFragment(mediaId: Int) = GlobalScope.launch {
-        val loadingFragment = LoadingFragment()
-        supportFragmentManager.commit {
-            add(R.id.nav_host_fragment, loadingFragment, "MediaFragment")
-            show(loadingFragment)
-        }
-
-        // load the streams for the selected media
-        val media = AoDParser.getMediaById(mediaId)
-        val tmdb = TMDBApiController().search(media.info.title, media.type)
-
-        val mediaFragment = MediaFragment(media, tmdb)
+        val mediaFragment = MediaFragment(mediaId)
         supportFragmentManager.commit {
             add(R.id.nav_host_fragment, mediaFragment, "MediaFragment")
             addToBackStack(null)
             show(mediaFragment)
-        }
-
-        supportFragmentManager.commit {
-            remove(loadingFragment)
         }
     }
 

app/src/main/java/org/mosad/teapod/ui/fragments/LoadingFragment.kt
@@ -1,16 +1,0 @@
-package org.mosad.teapod.ui.fragments
-
-import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import androidx.fragment.app.Fragment
-import org.mosad.teapod.R
-
-class LoadingFragment : Fragment() {
-
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.fragment_loading, container, false)
-    }
-
-}

app/src/main/java/org/mosad/teapod/ui/fragments/MediaFragment.kt
@@ -13,22 +13,26 @@ import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.bumptech.glide.request.RequestOptions
 import jp.wasabeef.glide.transformations.BlurTransformation
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import org.mosad.teapod.MainActivity
 import org.mosad.teapod.R
 import org.mosad.teapod.databinding.FragmentMediaBinding
 import org.mosad.teapod.parser.AoDParser
+import org.mosad.teapod.util.*
 import org.mosad.teapod.util.DataTypes.MediaType
-import org.mosad.teapod.util.Episode
-import org.mosad.teapod.util.Media
-import org.mosad.teapod.util.StorageController
-import org.mosad.teapod.util.TMDBResponse
 import org.mosad.teapod.util.adapter.EpisodeItemAdapter
 
-class MediaFragment(private val media: Media, private val tmdb: TMDBResponse) : Fragment() {
+class MediaFragment(private val mediaId: Int) : Fragment() {
 
     private lateinit var binding: FragmentMediaBinding
     private lateinit var adapterRecEpisodes: EpisodeItemAdapter
     private lateinit var viewManager: RecyclerView.LayoutManager
+
+    private lateinit var media: Media
+    private lateinit var tmdb: TMDBResponse
     private lateinit var nextEpisode: Episode
 
     companion object {
@@ -46,15 +50,24 @@ class MediaFragment(private val media: Media, private val tmdb: TMDBResponse) : 
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        binding.frameLoading.visibility = View.VISIBLE
 
-        initGUI()
-        initActions()
+        GlobalScope.launch {
+            // load the streams for the selected media
+            media = AoDParser.getMediaById(mediaId)
+            tmdb = TMDBApiController().search(media.info.title, media.type)
+
+            withContext(Dispatchers.Main) {
+                updateGUI()
+                initActions()
+            }
+        }
     }
 
     /**
      * if tmdb data is present, use it, else use the aod data
      */
-    private fun initGUI() {
+    private fun updateGUI() = with(binding) {
         // generic gui
         val backdropUrl = if (tmdb.backdropUrl.isNotEmpty()) tmdb.backdropUrl else media.info.posterUrl
         val posterUrl = if (tmdb.posterUrl.isNotEmpty()) tmdb.posterUrl else media.info.posterUrl
@@ -62,27 +75,27 @@ class MediaFragment(private val media: Media, private val tmdb: TMDBResponse) : 
         Glide.with(requireContext()).load(backdropUrl)
             .apply(RequestOptions.placeholderOf(ColorDrawable(Color.DKGRAY)))
             .apply(RequestOptions.bitmapTransform(BlurTransformation(20, 3)))
-            .into(binding.imageBackdrop)
+            .into(imageBackdrop)
 
         Glide.with(requireContext()).load(posterUrl)
-            .into(binding.imagePoster)
+            .into(imagePoster)
 
-        binding.textTitle.text = media.info.title
-        binding.textYear.text = media.info.year.toString()
-        binding.textAge.text = media.info.age.toString()
-        binding.textOverview.text = media.info.shortDesc
+        textTitle.text = media.info.title
+        textYear.text = media.info.year.toString()
+        textAge.text = media.info.age.toString()
+        textOverview.text = media.info.shortDesc
         if (StorageController.myList.contains(media.id)) {
-            Glide.with(requireContext()).load(R.drawable.ic_baseline_check_24).into(binding.imageMyListAction)
+            Glide.with(requireContext()).load(R.drawable.ic_baseline_check_24).into(imageMyListAction)
         } else {
-            Glide.with(requireContext()).load(R.drawable.ic_baseline_add_24).into(binding.imageMyListAction)
+            Glide.with(requireContext()).load(R.drawable.ic_baseline_add_24).into(imageMyListAction)
         }
 
         // specific gui
         if (media.type == MediaType.TVSHOW) {
             adapterRecEpisodes = EpisodeItemAdapter(media.episodes)
             viewManager = LinearLayoutManager(context)
-            binding.recyclerEpisodes.layoutManager = viewManager
-            binding.recyclerEpisodes.adapter = adapterRecEpisodes
+            recyclerEpisodes.layoutManager = viewManager
+            recyclerEpisodes.adapter = adapterRecEpisodes
 
             binding.textEpisodesOrRuntime.text = getString(R.string.text_episodes_count, media.info.episodesCount)
 
@@ -94,16 +107,18 @@ class MediaFragment(private val media: Media, private val tmdb: TMDBResponse) : 
             }
 
             // title is the next episodes title
-            binding.textTitle.text = nextEpisode.title
+            textTitle.text = nextEpisode.title
         } else if (media.type == MediaType.MOVIE) {
-            binding.recyclerEpisodes.visibility = View.GONE
+            recyclerEpisodes.visibility = View.GONE
 
             if (tmdb.runtime > 0) {
-                binding.textEpisodesOrRuntime.text = getString(R.string.text_runtime, tmdb.runtime)
+                textEpisodesOrRuntime.text = getString(R.string.text_runtime, tmdb.runtime)
             } else {
-                binding.textEpisodesOrRuntime.visibility = View.GONE
+                textEpisodesOrRuntime.visibility = View.GONE
             }
         }
+
+        frameLoading.visibility = View.GONE // hide loading indicator
     }
 
     private fun initActions() {
@@ -168,6 +183,5 @@ class MediaFragment(private val media: Media, private val tmdb: TMDBResponse) : 
         adapterRecEpisodes.updateWatchedState(true, media.episodes.indexOf(ep))
         adapterRecEpisodes.notifyDataSetChanged()
     }
-
 
 }

app/src/main/res/layout/fragment_loading.xml
@@ -1,17 +1,0 @@
-<?xml version="1.0" encoding="utf-8"?>
-<FrameLayout xmlns:android="http://schemas.android.com/apk/res/android"
-    xmlns:tools="http://schemas.android.com/tools"
-    android:layout_width="match_parent"
-    android:layout_height="match_parent"
-    android:background="?themePrimary"
-    android:clickable="true"
-    android:focusable="true">
-
-    <com.google.android.material.progressindicator.ProgressIndicator
-        android:id="@+id/progressBar2"
-        style="@style/Widget.MaterialComponents.ProgressIndicator.Circular.Indeterminate"
-        android:layout_width="70dp"
-        android:layout_height="70dp"
-        android:layout_gravity="center"
-        tools:visibility="visible" />
-</FrameLayout>

app/src/main/res/layout/fragment_media.xml
@@ -135,7 +135,8 @@
                         android:layout_width="48dp"
                         android:layout_height="48dp"
                         android:src="@drawable/ic_baseline_add_24"
-                        app:tint="?buttonBackground" />
+                        app:tint="?buttonBackground"
+                        android:contentDescription="@string/my_list" />
 
                     <TextView
                         android:id="@+id/text_my_list_action"
@@ -158,5 +159,21 @@
                 tools:listitem="@layout/item_episode" />
         </LinearLayout>
     </androidx.core.widget.NestedScrollView>
+
+    <FrameLayout
+        android:id="@+id/frame_loading"
+        android:layout_width="match_parent"
+        android:layout_height="match_parent"
+        android:background="?themePrimary"
+        android:visibility="gone">
+
+        <com.google.android.material.progressindicator.ProgressIndicator
+            android:id="@+id/loadingIndicator"
+            style="@style/Widget.MaterialComponents.ProgressIndicator.Circular.Indeterminate"
+            android:layout_width="70dp"
+            android:layout_height="70dp"
+            android:layout_gravity="center"
+            tools:visibility="visible" />
+    </FrameLayout>
 
 </FrameLayout>